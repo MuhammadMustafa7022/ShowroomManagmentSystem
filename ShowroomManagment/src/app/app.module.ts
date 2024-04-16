3d8e2c30d5ffcd324b66b0f3ef89c57e4401fa27
MuhammadMustafa7022 <muhammadmustafa7022@gmail.com>
2024-04-17 02:44:41 +0500

    [Dashboard Theme Integrate]

--- ShowroomManagment/src/app/app.module.ts
+++ ShowroomManagment/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './Dashboard/sidebar/sidebar.component';
+import { TopbarComponent } from './Dashboard/topbar/topbar.component';
+import { FooterComponent } from './Dashboard/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
