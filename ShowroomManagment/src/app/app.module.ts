a4763d56297692531d56e3ce18d2c3226a32fa4f
MuhammadMustafa7022 <muhammadmustafa7022@gmail.com>
2024-04-18 00:39:13 +0500

    Revert "[Dashboard Theme Integrate]"
    This reverts commit 3d8e2c30d5ffcd324b66b0f3ef89c57e4401fa27.

--- ShowroomManagment/src/app/app.module.ts
+++ ShowroomManagment/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './Dashboard/sidebar/sidebar.component';
-import { TopbarComponent } from './Dashboard/topbar/topbar.component';
-import { FooterComponent } from './Dashboard/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    TopbarComponent,
-    FooterComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
