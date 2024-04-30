4393eef7dc69802fb180672835265e34ea02aa65
MuhammadMustafa7022 <muhammadmustafa7022@gmail.com>
2024-05-01 01:07:23 +0500

    Department Read,Create,Delete Complete in Angular

--- ShowroomManagment/src/app/Dashboard/department/department.component.ts
+++ ShowroomManagment/src/app/Dashboard/department/department.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DepartmentService } from '../../services/department.service';
-import { response } from 'express';
+// import { response } from 'express';
+import { Department } from '../../shared/department.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-department',
@@ -10,18 +12,85 @@ import { response } from 'express';
 })
 export class DepartmentComponent {
 
-  alldepartment:any[] = [];
+alldepartment:any[] = [];
 
-constructor(private apidepartment:DepartmentService) {}
+constructor(public apidepartment:DepartmentService) {}
 
   ngOnInit(): void {
    this.apidepartment.getDepartment().subscribe((response:any) =>{
-    console.log(response.Response);
-    this.alldepartment = response.Response;
-    console.log(this.alldepartment);      
-   } )
-
+   this.alldepartment = response.Response;
+   });
   }
   
+  
+  EditDepartment(SelectedDepartment:Department)
+  {
+    console.log(SelectedDepartment);
+    this.apidepartment.DepartmentData = SelectedDepartment;
+  }
+  
+  DeleteDepartment(Id:number)
+  {
+    if(confirm('Are You Really Want To Delete This Record?'))
+      {
+        this.apidepartment.DeleteDepartment(Id).subscribe((response:any)=> {
+          alert('Record Deleted...');
+          //reload data in list
+          this.apidepartment.getDepartment().subscribe((response:any)=>{
+            this.alldepartment = response.Response;
+          });
+        },
+        err=>{
+          console.log('Record Not Deleted...');
+        }
+      )
+    }
+  }
+
+
+  Submit(form:NgForm)
+  {
+    if(this.apidepartment.DepartmentData.id == 0)
+      {
+        this.InsertDepartment(form);
+      }
+      else
+      {
+        this.UpdateDepartment(form);
+      }
+  }
+
+
+
+  InsertDepartment(form:NgForm)
+  {
+    this.apidepartment.SaveDepartment().subscribe(d=> {
+      this.ResetForm(form);
+      this.refreshdata();
+      console.log("save changes");
+     });
+  }
+
+  UpdateDepartment(form:NgForm)
+  {
+    this.apidepartment.UpdateDepartment().subscribe(d=>{
+      this.ResetForm(form);
+      this.refreshdata();
+      console.log("save changes");
+  });
+  }
+
+  ResetForm(form:NgForm)
+  {
+    form.form.reset();
+    this.apidepartment.DepartmentData = new Department();
+  }
+
+  refreshdata()
+  {
+    this.apidepartment.getDepartment().subscribe((response:any) =>{
+      this.alldepartment = response.Response;
+      });
+  }
 
 }
--- ShowroomManagment/src/app/services/department.service.ts
+++ ShowroomManagment/src/app/services/department.service.ts
@@ -2,18 +2,36 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import { environment } from '../../environments/environment.development'
 import { HttpClient } from '@angular/common/http';
+import { Department } from '../shared/department.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DepartmentService {
+export class DepartmentService { 
   private apiUrl = environment.apiUrl;
   constructor(private client:HttpClient) { }
+
+ DepartmentData:Department = new Department(); // for post data / insert data
 
   getDepartment():any
   {
     // return this.client.get(this.url);
-    return this.client.get(`${this.apiUrl}/department/GetDepartments`);
+    return this.client.get(`${this.apiUrl}/Department/GetDepartments`);
+  }
+
+  SaveDepartment()
+  {
+    return this.client.post(`${this.apiUrl}/Department/AddDepartment`,this.DepartmentData);
+  }
+
+  UpdateDepartment()
+  {
+    return this.client.post(`${this.apiUrl}/Department/UpdateDepartment`,this.DepartmentData);
+  }
+
+  DeleteDepartment(Id:number)
+  {
+    return this.client.get(`${this.apiUrl}/Department/DeleteDepartment/${Id}`);
   }
 
   // getDepartment(): Observable<any> {
